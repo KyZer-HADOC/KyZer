e77ccc608f86771326964cfa7143d39de70d84ba
KyZer-HADOC <nesarathathsinduramasinghe@gmail.com>
2026-07-24 15:41:44 +0530

    Set default SESSION_ID in config.js
    Update SESSION_ID to a default value for development.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "TuxABDhL#1470bpkl52V4mTmDbzsnkpXGgBR_kS6vV_mQrfYllkA",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/KyZer-HADOC/KyZer/blob/main/images/KyZer%20bot_%20gothic%20cyberpunk%20aesthetic.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 KyZer-Fea Is Alive Now😍*",
 BOT_OWNER: '94716252002',  // Replace with the owner's phone number
