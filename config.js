794c5361776cc87a753126ae5c60cab4359f5768
KyZer-HADOC <nesarathathsinduramasinghe@gmail.com>
2026-07-28 19:56:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "f6Y3GK7D#VAg30M6hWaJT7QJ8_-z_IiArG_kkVjx0PyhM8O8KCbk",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/KyZer-HADOC/KyZer/blob/main/images/KyZer%20bot_%20gothic%20cyberpunk%20aesthetic.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 KyZer-Fea Is Alive Now😍*",
-BOT_OWNER: '94716252002',  // Replace with the owner's phone number
+BOT_OWNER: '94718354800',  // Replace with the owner's phone number
 AUTO_STATUS_SEEN: 'true',
 AUTO_STATUS_REACT: 'true',
 
