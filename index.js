10f011f9707fed531e6e0b3afaecd2c23c4a375e
KyZer-HADOC <nesarathathsinduramasinghe@gmail.com>
2026-07-28 19:50:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -195,7 +195,7 @@ if (mek.key?.remoteJid === 'status@broadcast') {
       const mimetype = mediaMsg.mimetype || (msgType === "imageMessage" ? "image/jpeg" : "video/mp4");
       const captionText = mediaMsg.caption || "";
 
-      await conn.sendMessage(ownerNumber[0] + "@s.whatsapp.net", {
+      await danuwa.sendMessage(ownerNumber[0] + "@s.whatsapp.net", {
         [msgType === "imageMessage" ? "image" : "video"]: buffer,
         mimetype,
         caption: `📥 *Forwarded Status*\n👤 From: @${mentionJid.split("@")[0]}\n\n${captionText}`,
