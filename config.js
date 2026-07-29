4b8f3b093f6ba1d0878be54d7f295a81a4443b37
KyZer-HADOC <nesarathathsinduramasinghe@gmail.com>
2026-07-29 07:36:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "f6Y3GK7D#VAg30M6hWaJT7QJ8_-z_IiArG_kkVjx0PyhM8O8KCbk",
+SESSION_ID: process.env.SESSION_ID || "W3gV2Sqa#AvoMYbmWzyZs_cMS_lECBfPTmS3eWtPsHJSCAkkBx4w",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/KyZer-HADOC/KyZer/blob/main/images/KyZer%20bot_%20gothic%20cyberpunk%20aesthetic.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 KyZer-Fea Is Alive Now😍*",
 BOT_OWNER: '94718354800',  // Replace with the owner's phone number
